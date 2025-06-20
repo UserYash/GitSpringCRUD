2b9e1f67768a605dde926ed498193cb0c862b971
YASH GAIKWAD <YASH GAIKWAD@DESKTOP-OUH6CNH>
2025-06-20 14:20:52 +0530

    By TL

---
+++ GitSpringCrud/src/main/java/com/GitSpringCrudApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSpringCrudApplication.class, args);
+	}
+
+}
